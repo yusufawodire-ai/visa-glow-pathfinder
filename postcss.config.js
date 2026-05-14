d765a2f7c2749ceae35c56af0c9a047de1b54247
yusuf <yusuf.awodire@innovativeautomations.dev>
2026-05-14 15:34:08 +0100

    Show thank-you message on submit instead of routing to result page

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+
